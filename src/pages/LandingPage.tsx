9b312b1245d31a8081dfe38b9ef7d462beb16112
KanonKC <kanonkce@gmail.com>
2024-10-23 19:53:20 +0700

    Clear unused variable

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,18 +1,9 @@
 import Navbar from "@/layouts/Navbar/Navbar"
-import { Button } from "@/components/ui/button"
-import { useAppDispatch, useAppSelector } from "@/stores/hooks"
-import { useNavigate } from "react-router-dom"
 import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 
 const LandingPage = () => {
     
-    const count = useAppSelector(state => state.counter.value)
-    const dispatch = useAppDispatch()
-
-    const handleOnClickIncrement = () => {
-        dispatch({type: 'counter/incrementByAmount', payload: 3})
-    }
-
     const nagivate = useNavigate()
 
     useEffect(() => {
